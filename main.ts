07f0968a7c46bd833ee5b96ce3c3b9c5f3b495e2
Giorgos Petkakis <85838659+giorgospetkakis@users.noreply.github.com>
2023-12-22 09:03:43 +0100

    More tests with the plugin

--- main.ts
+++ main.ts
@@ -1,16 +1,4 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Menu,
-	MenuItem,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
-
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface WriteBetterSettings {
 	mySetting: string;
@@ -20,6 +8,78 @@ const DEFAULT_SETTINGS: WriteBetterSettings = {
 	mySetting: "default",
 };
 
+import { syntaxTree } from "@codemirror/language";
+import { RangeSetBuilder } from "@codemirror/state";
+import {
+	Decoration,
+	DecorationSet,
+	EditorView,
+	PluginSpec,
+	PluginValue,
+	ViewPlugin,
+	ViewUpdate,
+	WidgetType,
+} from "@codemirror/view";
+
+
+export class AnnotationWidget extends WidgetType {
+  toDOM(view: EditorView): HTMLElement {
+    const div = document.createElement("span");
+    div.className = "tooltip";
+
+    return div;
+  }
+}
+
+class EmojiListPlugin implements PluginValue {
+	decorations: DecorationSet;
+
+	constructor(view: EditorView) {
+		this.decorations = this.buildDecorations(view);
+	}
+
+	update(update: ViewUpdate) {
+		if (update.docChanged || update.viewportChanged) {
+			this.decorations = this.buildDecorations(update.view);
+		}
+	}
+
+	destroy() {}
+
+	buildDecorations(view: EditorView): DecorationSet {
+		const builder = new RangeSetBuilder<Decoration>();
+
+		for (let { from, to } of view.visibleRanges) {
+			syntaxTree(view.state).iterate({
+				from,
+				to,
+				enter(node) {
+                    if (!node.type.name.startsWith("tooltip"))
+                    {
+                        builder.add(node.from, node.to, Decoration.mark({ 
+                            class: "annotated",
+                        }));
+                        // builder.add(node.from, node.to, Decoration.widget({
+                        //     widget: new AnnotationWidget(),
+                        // }));
+                    }
+				},
+			});
+		}
+
+		return builder.finish();
+	}
+}
+
+const pluginSpec: PluginSpec<EmojiListPlugin> = {
+	decorations: (value: EmojiListPlugin) => value.decorations,
+};
+
+export const emojiListPlugin = ViewPlugin.fromClass(
+	EmojiListPlugin,
+	pluginSpec
+);
+
 export default class WriteBetter extends Plugin {
 	settings: WriteBetterSettings;
 
@@ -27,55 +87,7 @@ export default class WriteBetter extends Plugin {
 		console.log("Loading plugin...");
 
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"pencil",
-			"View Write Better stats",
-			(evt: MouseEvent) => {
-				new WriteBetterModal(this.app).open();
-			}
-		);
-
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		var warningCount = 0;
-		const warningCounter = this.addStatusBarItem();
-		warningCounter.setText("⚠ " + warningCount.toString());
-		warningCounter.addClass("warning-count");
-		warningCounter.onClickEvent((evt) => {
-			warningCount++;
-			warningCounter.setText("⚠ " + warningCount.toString());
-		});
-
-		// COMMANDS
-
-		this.addCommand({
-			id: "write-better-fix-selection",
-			name: "Fix selection",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-
-		this.addCommand({
-			id: "write-better-fix-all",
-			name: "Fix all (may take a while)",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-
-		this.addCommand({
-			id: "write-better-modal",
-			name: "Display Write Better Modal",
-			callback: () => {
-				new WriteBetterModal(this.app).open();
-			},
-		});
+		this.registerEditorExtension(emojiListPlugin);
 
 		// SETTINGS
 
@@ -96,22 +108,6 @@ export default class WriteBetter extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class WriteBetterModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Under construction...");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
